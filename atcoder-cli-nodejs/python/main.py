1f2e28f7732498c460908e746e5869614747d196
minarin0179 <minarin0179@gmail.com>
2024-05-22 17:34:49 +0000

    だいたいできた

---
+++ atcoder-cli-nodejs/python/main.py
@@ -0,0 +1,11 @@
+def solve():
+    N = int(input())
+    N, M = map(int, input().split()) 
+    A = list(map(int, input().split())) # 1xN
+    B = [int(input()) for _ in range(N)] # Nx1
+    C = [list(map(int, input().split())) for _ in range(N)] # NxM
+
+    
+
+if __name__ == "__main__": 
+    solve()
